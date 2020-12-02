500717a5704c12031d964209daf2e84b71e44f28
Gunnar Grop <gunnar.grop@protonmail.com>
2020-12-02 02:46:47 +0100

    Added some nice new charts!

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,90 @@
 import './App.css';
-import ChartLine from './charts/LineChart.js';
+import Chart from './components/chart';
+import {Line, Bar, Pie} from 'react-chartjs-2';
+
+const chartData1 = {
+    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
+    datasets: [
+        {
+            label: 'MB',
+            data: [
+                0,
+                12,
+                20,
+                47,
+                5,
+                18,
+                36
+            ],
+            backgroundColor: [
+                '#54ffdd'
+            ]
+        }
+    ]
+};
+
+const chartData2 = {
+    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
+    datasets: [
+        {
+            label: 'Core 1',
+            data: [
+                99,
+                87,
+                86,
+                67,
+                55,
+                23,
+                28
+            ],
+            backgroundColor: [
+                '#54ffdd',
+            ]
+        },
+        {
+            label: 'Core 2',
+            data: [
+                0,
+                12,
+                20,
+                47,
+                5,
+                18,
+                36
+            ],
+            backgroundColor: [
+                '#ebb'
+            ]
+        }
+    ]
+};
+
+const chartData3 = {
+    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
+    datasets: [
+        {
+            label: 'Mb/s',
+            data: [
+                99,
+                87,
+                86,
+                67,
+                55,
+                23,
+                28
+            ],
+            backgroundColor: [
+                '#54ffdd',
+                '#6cffa9',
+                '#fdff78',
+                '#ff8742',
+                '#ff7ef2',
+                '#ffc0c6',
+                '#c7d6ff'
+            ]
+        }
+    ]
+};
 
 function App() {
     return (
@@ -11,9 +96,9 @@ function App() {
             <div className="Button">Virtual machine</div>
           </div>
           <div className="ChartContainer">
-            <ChartLine />
-            <ChartLine />
-            <ChartLine />
+            <Chart chartData={chartData1} type={Line} titleText='RAM Usage' />
+            <Chart chartData={chartData2} type={Line} />
+            <Chart chartData={chartData3} type={Bar} titleText='Network throughput'/>
           </div>
         </div>
     );
---
+++ dashboard/src/components/chart.js
@@ -0,0 +1,67 @@
+// @ts-check
+
+// @ts-ignore
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
+
+/**
+ * Class to create charts of diffrent types
+ */
+class Chart extends Component {
+    /**
+     * @param {Object} props Properties passed from caller
+     * @param {Object} props.chartData - Data used to construct the chart
+     * @param {string[]} props.chartData.labels - Labels for the x-axis
+     * @param {Object[]} props.chartData.datasets - The diffrent datasets to display on the chart (can contain several datasets)
+     * @param {string} props.chartData.datasets[].label - Label to use for the dataset
+     * @param {number[]} props.chartData.datasets[].data - Literal data of the dataset
+     * @param {string[]} props.chartData.datasets[].backgroundColor - Colors to use for the dataset (can contain several colors)
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: props.chartData
+        };
+    }
+
+    /**
+     * Deafult values for select chart properties. These values can be overridden
+     * @type {{type: Object, titleDisplay: boolean, titleText: string, titleSize: number, titleColor: string, legendDisplay: boolean, legendPosition: string}}
+     */
+    static defaultProps = {
+        type: Line,
+        titleDisplay: true,
+        titleText: 'CPU Utilization',
+        titleSize: 18,
+        titleColor: '#FFF',
+        legendDisplay: true,
+        legendPosition: 'bottom',
+    };
+
+    /**
+     * Renders chart to the screen
+     */
+    render() {
+        return(
+            <div className="ChartItem">
+              <this.props.type
+                data = {this.state.chartData}
+                options = {{
+                    title: {
+                        display: this.props.titleDisplay,
+                        text: this.props.titleText,
+                        fontSize: this.props.titleSize,
+                        fontColor: this.props.titleColor
+                    },
+                    legend: {
+                        display: this.props.legendDisplay,
+                        position: this.props.legendPosition
+                    }
+                }}
+              />
+            </div>
+        );
+    }
+}
+
+export default Chart;
