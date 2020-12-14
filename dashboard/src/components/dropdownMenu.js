2c12e03dc899169e193640d1dedda1ca30517897
Gunnar Grop <gunnar.grop@protonmail.com>
2020-12-14 23:37:12 +0100

    Created a generic dropdown menu that can be populated from the caller

--- dashboard/src/components/dropdownMenu.js
+++ dashboard/src/components/dropdownMenu.js
@@ -1,34 +1,37 @@
 // @ts-check
 
 // @ts-ignore
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import {RiArrowDownSLine} from 'react-icons/ri';
+
+
 
 /**
- * A generic dropdown menu
+ * A generic dropdown menu which takes it's text and menu items as properties in the constructor.
  */
 class DropdownMenu extends Component {
     /**
-     * @param {Object} args - Properties passed from caller.
+     * @param{object} args - Properties passed from the caller
+     * @param{boolean} args.showMenu - Defaults as false.
+     * @param{object} args.menuItems - The menu items that will be shown in the dropdown.
+     * @param{object[]} args.menuItems[] - An array of items.
+     * @param{string} args.menuItems[].link - The link that the menu item will open.
+     * @param{string} args.menuItems[].text - The text that will be displayed on the menu item.
      */
     constructor(args) {
         super(args);
         this.state = {
-            toggleMenu: false
+            showMenu: false,
+            menuItems: args.menuItems
         };
-
-        this.toggleMenu = this.toggleMenu.bind(this);
     }
 
+    /**
+     * Deafult properties. These can be overridden.
+     * @type{{text: string}}
+     */
     static defaultProps = {
         text: "Dropdown menu"
-    }
-
-    toggleMenu(event) {
-        event.preventDefault();
-
-        this.setState({
-            toggleMenu: true
-        });
     }
 
     /**
@@ -36,27 +39,27 @@ class DropdownMenu extends Component {
      */
     render() {
         return(
-            <div className="dropdownMenu">
-              <button onClick={this.toggleMenu}>{this.props.text}</button>
+            <div>
+              <button onClick={(e) => this.setState({showMenu: !this.state.showMenu})}>{this.props.text}  <RiArrowDownSLine /></button>
               {
-                  this.state.toggleMenu
-                      ? (
-                          <div className="Items">
-                            {
-                                /*this.props.items.map((obj, i) => {
-                                    return(
-                                        <button key={i}>{obj}</button>
-                                    );
-                                    })*/
-
-                            }
-                            <button>Menu item 1</button>
-                            <button>Menu item 2</button>
-                            <button>Menu item 3</button>
+                  this.state.showMenu ? (
+                          <div className="Dropdown">
+                            <ul>
+                              {
+                                  this.state.menuItems.map((obj, i) => {
+                                      return ( <li key={i}>
+                                                 <a href={obj.link}>
+                                                   {obj.text}
+                                                 </a>
+                                               </li> );
+                                  })
+                              }
+                            </ul>
                           </div>
+                      ) : (
+                          null
                       )
-                      : (null)
-              };
+              }
             </div>
         );
     }
