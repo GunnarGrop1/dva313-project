d9ba44fc4906027bab79c278f421013ad7fec59f
Gunnar Grop <gunnar.grop@protonmail.com>
2020-12-15 22:41:34 +0100

    Added functionality for different pages through react-router-dom. Just add page information into the "links" array

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,104 +1,37 @@
 import './App.css';
-import Chart from './components/chart';
-import {Line, Bar, Pie} from 'react-chartjs-2';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AiOutlineDashboard, AiOutlineSetting} from 'react-icons/ai';
 import GlobalMenuBar from './components/globalMenuBar';
-import DashboardMenu from './components/dashboardMenu';
+import Dashboard from './Dashboard';
+import Settings from './Settings';
 
-const chartData1 = {
-    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
-    datasets: [
-        {
-            label: 'MB',
-            data: [
-                0,
-                12,
-                20,
-                47,
-                5,
-                18,
-                36
-            ],
-            backgroundColor: [
-                '#54ffdd'
-            ]
-        }
-    ]
-};
-
-const chartData2 = {
-    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
-    datasets: [
-        {
-            label: 'Core 1',
-            data: [
-                99,
-                87,
-                86,
-                67,
-                55,
-                23,
-                28
-            ],
-            backgroundColor: [
-                '#54ffdd',
-            ]
-        },
-        {
-            label: 'Core 2',
-            data: [
-                0,
-                12,
-                20,
-                47,
-                5,
-                18,
-                36
-            ],
-            backgroundColor: [
-                '#ebb'
-            ]
-        }
-    ]
-};
-
-const chartData3 = {
-    labels: ['13:00', '13:05', '13:10', '13:15', '13:20', '13:25', '13:30'],
-    datasets: [
-        {
-            label: 'Mb/s',
-            data: [
-                99,
-                87,
-                86,
-                67,
-                55,
-                23,
-                28
-            ],
-            backgroundColor: [
-                '#54ffdd',
-                '#6cffa9',
-                '#fdff78',
-                '#ff8742',
-                '#ff7ef2',
-                '#ffc0c6',
-                '#c7d6ff'
-            ]
-        }
-    ]
-};
+const links = [
+    {
+        component: Dashboard,
+        path: "/",
+        icon: AiOutlineDashboard
+    },
+    {
+        component: Settings,
+        path: "/settings",
+        icon: AiOutlineSetting
+    }
+];
 
 function App() {
     return (
-        <div className="App">
-          <GlobalMenuBar />
-          <DashboardMenu />
-          <div className="ChartContainer">
-            <Chart chartData={chartData1} type={Line} titleText='RAM Usage' />
-            <Chart chartData={chartData2} type={Line} />
-            <Chart chartData={chartData3} type={Bar} titleText='Network throughput'/>
+        <BrowserRouter>
+          <div className="App">
+            <GlobalMenuBar links={links} />
+            <Switch>
+              {
+                  links.map((obj, i) => {
+                      return (<Route path={obj.path} exact component={obj.component}/>);
+                  })
+              }
+            </Switch>
           </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
